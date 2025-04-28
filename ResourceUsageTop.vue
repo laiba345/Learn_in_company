1eba4fbe22c0305d1471273423c7eaf2c957e46a
来杯黑美式 <51855280+laiba345@users.noreply.github.com>
2025-04-28 13:51:59 +0800

    Update ResourceUsageTop.vue

--- ResourceUsageTop.vue
+++ ResourceUsageTop.vue
@@ -92,3 +92,93 @@ export default {
 };
 </script>
 ```
+
+## 原先的
+```
+<template>
+  <div id="chart-container2" style="width: 62vw; height: 400px;"></div>
+</template>
+
+<script>
+import * as echarts from 'echarts';
+
+export default {
+  name: 'ResourceUsageChart',
+  mounted() {
+    this.initChart();
+  },
+  methods: {
+    initChart() {
+      const chartDom = document.getElementById('chart-container2');
+      const myChart = echarts.init(chartDom);
+      const option = {
+        tooltip: {
+          trigger: 'axis'
+        },
+        legend: {
+          data: ['YBK1', 'YBK2', 'YBK3', 'YBK4', 'YBK5'],
+          bottom: '1%'
+        },
+        grid: {
+          top: '6%',
+          left: '3%',
+          right: '2%',
+          bottom: '8%',
+          containLabel: true
+        },
+        xAxis: {
+          type: 'category',
+          boundaryGap: false,
+          data: ['10:00', '12:00', '14:00', '16:00', '18:00', '20:00', '22:00', '00:00', '2:00', '4:00', '6:00', '8:00', '10:00'], 
+          axisTick: {
+            show: false
+          }
+        },
+        yAxis: {
+          type: 'value',
+          min: 0,
+          max: 100,
+          interval: 20,
+          axisLabel: {
+            formatter: '{value}%'
+          }
+        },
+        series: [
+          {
+            name: 'YBK1',
+            type: 'line',
+            symbol: 'none',
+            data: [5, 30, 38, 35, 28, 30, 32, 33, 33, 33, 33, 33, 35]
+          },
+          {
+            name: 'YBK2',
+            type: 'line',
+            symbol: 'none',
+            data: [25, 27, 28, 28, 35, 30, 28, 28, 28, 28, 15, 10, 3]
+          },
+          {
+            name: 'YBK3',
+            type: 'line',
+            symbol: 'none',
+            data: [23, 20, 10, 20, 25, 23, 25, 28, 30, 30, 30, 30, 30]
+          },
+          {
+            name: 'YBK4',
+            type: 'line',
+            symbol: 'none',
+            data: [15, 18, 15, 25, 28, 10, 3, 5, 5, 10, 15, 13, 10]
+          },
+          {
+            name: 'YBK5',
+            type: 'line',
+            symbol: 'none',
+            data: [5, 0, 0, 15, 20, 10, 5, 10, 18, 15, 5, 10, 10]
+          }
+        ]
+      };
+      option && myChart.setOption(option);
+    }
+  }
+};
+</script>
+```
